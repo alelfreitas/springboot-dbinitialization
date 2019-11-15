8bfe90bdaab42e788e8113b2066135cf4595f481
Alexandre <allezito@gmail.com>
2019-11-15 14:16:33 -0300

    Testes remocao de configuracoes do hikari e jpa

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,9 +5,14 @@ create database dbtest;
 
 use dbtest;
 
-DROP TABLE IF EXISTS `person`;
-CREATE TABLE `person` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+DROP TABLE IF EXISTS `user_details`;
+CREATE TABLE `user_details` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `status` tinyint(10) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10001 ;
